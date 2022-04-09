62b6ef53083071e1f6e76a4f526dceaae72763bc
LukasSchneider1 <79796755+LukasSchneider1@users.noreply.github.com>
2022-04-09 14:49:50 +0200

    08 erledigt

---
+++ JS/08_Switch_Statement.js
@@ -0,0 +1,31 @@
+// Erstelle eine Zufallszahl zwischen 5 und 10
+var min = 5;
+var max = 10;
+let random = Math.round(Math.random() * (max - min) + min);
+1;
+console.log(random);
+// Wenn der Wert 10 ist gib aus Ten
+// Wenn der Wert 9 ist gib aus Nine
+// Wenn der Wert 8 ist gib aus Eight
+// etc.
+
+switch (random) {
+  case 10:
+    console.log("Ten");
+    break;
+  case 9:
+    console.log("Nine");
+    break;
+  case 8:
+    console.log("Eight");
+    break;
+  case 7:
+    console.log("Seven");
+    break;
+  case 6:
+    console.log("Six");
+    break;
+  case 5:
+    console.log("Five");
+    break;
+}
