3d1cea018c237588556eb229063ab1aaf02d5f75
LukasSchneider1 <79796755+LukasSchneider1@users.noreply.github.com>
2022-04-09 14:25:02 +0200

    02 - 04 erledigt

---
+++ JS/02_Strings_Zahlen.js
@@ -0,0 +1,14 @@
+//1) Rechnung
+let a = 17.4
+let b = 3.9
+
+let result = (a / b)
+
+console.log("The result of " + a + " divided by " + b + " is " + result)
+
+//2) Namenszuweisung
+
+let firstname = "Lukas"
+let lastname = "Schneider"
+
+console.log("Ich heiße " + firstname + " " + lastname)
---
+++ JS/03_Aufgaben_zu_Variablen.js
@@ -0,0 +1,29 @@
+// Erstelle eine Variable banana mit dem Wert "Banane"
+let banana = "Banane"
+// Erstelle eine Variable apple mit dem Wert "Apple"
+let apple = "Apple"
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+let bananaPricePerKilo = 2.14
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+let applePricePerKilo = 3.43
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
+// Preis von 8 Äpfeln
+let gewicht = 8 * 0.34
+let Preis = applePricePerKilo * gewicht
+console.log(Preis)
+
+// Preis von 1 Tonne Äpfel
+gewicht = 1000
+Preis = applePricePerKilo * gewicht
+console.log(Preis)
+// Preis von 17 Bananen
+gewicht = 17 * 0.22
+Preis = bananaPricePerKilo * gewicht
+console.log(Preis)
+// Preis von 1 Tonne Bananen
+gewicht = 1000
+Preis = bananaPricePerKilo * gewicht
+console.log(Preis)
