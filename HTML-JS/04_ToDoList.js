2bd0bca9b68b35b5a13e2c8f9c2f6573e749ddd7
LukasSchneider1 <79796755+LukasSchneider1@users.noreply.github.com>
2022-04-19 10:37:20 +0200

    04 erledigt

--- HTML-JS/04_ToDoList.js
+++ HTML-JS/04_ToDoList.js
@@ -1,4 +1,45 @@
+
+
+const task = []
+
+printTaskList();
+
+function getTask(){
+    let html = "";
+    let index = 0;
+    task.forEach(element => {
+        let checked = ""
+        if(element.isDone){
+            checked = "checked"
+        }
+        html += "<li>"+ element.aufgabe + " - " + element.responsible + " " + "<input name='checkbox' onclick='markTask(this)' data-index='" + index + "' type='checkbox'" + checked + "/></li>"
+        index++
+    });
+    return html
+}
+
+function markTask(element){
+    let index = element.attributes["data-index"].value
+    let checked = element.checked
+
+    task[index].isDone = checked;
+
+    printTaskList()
+}
+
+function printTaskList(){
+    document.getElementById("TaskList").innerHTML = getTask();
+}
+
+let aufgabe = ""
+
 document.getElementById("addTask").addEventListener("click", function(){
-    alert("Hello")
+    aufgabe = document.getElementById("aufgabe").value
+    zuständig = document.getElementById("responsible").value
+    
+    var task1 = {aufgabe: aufgabe, isDone: false, responsible: zuständig}
+    task.push(task1)
+    printTaskList()
 })
+
 
