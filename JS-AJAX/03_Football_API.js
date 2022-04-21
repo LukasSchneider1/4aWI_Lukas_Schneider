3f57e14538e259782f57b0eaac19583fae27e371
LukasSchneider1 <79796755+LukasSchneider1@users.noreply.github.com>
2022-04-21 14:46:58 +0200

    added teams

--- JS-AJAX/03_Football_API.js
+++ JS-AJAX/03_Football_API.js
@@ -1,6 +1,6 @@
 let teams = "https://api.football-data.org/v2/competitions/2002/teams"
 
-loadTeams()
+loadTeams();
 
 function loadTeams(){
     fetch(teams, {
@@ -14,11 +14,32 @@ function loadTeams(){
     .then(function (data) {
       let html = ""
       data.teams.forEach(element => {
-          html += "<li><img src='" + element.crestUrl + "'/>"+ element.name + "</li>";
+        html += "<div class='container'><div class='logo'><img src='" + element.crestUrl + "' width='150px' height='150px'></div><div class='team'>" + element.name + "</div><div class='website'><button class='viewWebsite'><a href='"+ element.website +"'>VIEW</a></button></div></div>"          
       });
-      document.getElementById("list").innerHTML = html;
+      document.getElementById("wrapper").innerHTML = html;
     })
     .catch(function (err) {
       console.log(err);
     });
+}
+
+function loadSeason(){
+  fetch(teams, {
+      headers:{
+          "x-auth-token": "819babcd7902454f930c154272296d78"
+      }
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let html = ""
+    
+        html += "<li>" + data.competition.code + "</li>";
+    
+    document.getElementById("list").innerHTML = html;
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 }
