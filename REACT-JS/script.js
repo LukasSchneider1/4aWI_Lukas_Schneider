e8bf546b3ec1fd11f1f45dc5646eec1b1e1c7506
LukasSchneider1 <79796755+LukasSchneider1@users.noreply.github.com>
2022-04-28 09:34:39 +0200

    01 -04 DONE in REACT

--- REACT-JS/script.js
+++ REACT-JS/script.js
@@ -1,1 +1,3 @@
-console.log("Hello");
+const sizeOf = require('image-size')
+const dimensions = sizeOf('C:\\Users\\lukas\\Pictures\\vlcsnap-2020-04-18-20h26m55s388.png')
+console.log(dimensions.width, dimensions.height)
---
+++ reactdemo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ButtonRow from './components/ButtonRow';
+import SuperButton from './components/SuperButton';
+
+function App() {
+  return (
+    <div className="App">
+      BUTTON
+     <SuperButton/>
+     <ButtonRow/>
+    </div>
+  );
+}
+
+export default App;
